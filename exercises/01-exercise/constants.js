9424f2b6365628433bfc9ae98d57970f5d1e9353
Jose Valenzuela <guadix88@gmail.com>
2022-09-02 14:40:14 +0200

    delete duplicated folder

--- exercises/01-exercise/constants.js
+++
@@ -1,3 +0,0 @@
-const BASE_NUM = 15;
-
-module.exports = BASE_NUM;
--- exercises/03-exercise/user-data.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  id: "c6619287d628844dc5b3",
-  firstName: "Nicholas Flatley",
-  lastName: "Pfeffer",
-  email: "Alyson39@yahoo.com",
-};
